4fd6c1706abad7a3b550e20b671209cfaf6faf71
yktoledo <yktoledo@hotmail.com>
2016-12-29 13:46:57 -0500

    first commit

---
+++ app/src/test/java/com/example/travissimple/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.travissimple;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(3, 2 + 2);
+    }
+}
